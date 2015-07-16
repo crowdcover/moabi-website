2008da6d96224b8ab353bd5218092a91067b73e0
Stadl0r <erik@flintgame.com>
2015-07-16 12:19:13 -0400

    Who we are change

--- archive-team.php
+++ archive-team.php
@@ -6,12 +6,12 @@
 <div class="content" id="intro">
   <div class="row text-center wp2">
     <h1>Who we Are</h1>
-    <p class="headline">We are a team of cartographers, coders, forest monitors, GIS experts to gender specialists, econometric modelers. Our team spans three continents working to build simple and robust technology.</p>
+    <p class="headline">We are a team of cartographers, coders, forest monitors, GIS experts, gender specialists and econometric modelers. Our team spans three continents and works to build simple yet robust technology, train local organizations and expand our collaborations. </p>
   </div>
   <div class="row">
     <div class="large-12 columns">
      <?php while ( have_posts() ) : the_post(); ?>
-			
+
 
 	      <div class="divider"></div>
 		  <div class="row grey collapse">
@@ -20,10 +20,10 @@
 		      <p><span><?php the_field('company_role'); ?></span></p>
 		      <p><?php the_field('bio_excerpt'); ?></p>
 		      <p><a href="<?php echo get_permalink(); ?>">Read More</a></p>
-		      
+
 		    </div>
 		    <div class="large-6 columns text-center " style="background: url('<?php the_field('headshot'); ?>');height: 400px;background-size:cover">
-		      
+
 		    </div>
 		  </div>
    		<?php endwhile; // end of the loop. ?>
