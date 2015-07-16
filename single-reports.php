72121d2cc1df584143720d579950b9f3c7e5c08d
Stadl0r <erik@flintgame.com>
2015-07-16 12:48:24 -0400

    report post type fix

--- single-reports.php
+++ single-reports.php
@@ -14,12 +14,12 @@
 
 </div>
 <div class="content" style="background: url('<?php the_field('project_bg'); ?>');background-size:cover;">
-  
+
   <div class="row" style="background: #fff; padding: 60px">
     <div class="large-12 columns">
       <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-          <?php the_field('project_description'); ?>
+          <?php the_content(); ?>
 
           <?php endwhile; ?>
     </div>
