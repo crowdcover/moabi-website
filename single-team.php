948fab08595cc69330f5623da8cf4f220585afb3
Stadl0r <erik@flintgame.com>
2015-07-20 14:59:17 -0400

    team page template layout

--- single-team.php
+++ single-team.php
@@ -4,7 +4,7 @@
 
 </div>
 <div class="content" id="intro">
-  
+
   <div class="row">
     <div class="large-12 columns">
      <?php while ( have_posts() ) : the_post(); ?>
@@ -13,18 +13,13 @@
       <p><span><?php the_field('company_role'); ?></span></p>
       </center>
       <div class="row grey collapse">
-
-        <div class="large-12 columns text-center " style="background: url('<?php the_field('headshot'); ?>');height: 300px;background-size:cover"></div>
-        <div class="large-12 columns" style="padding: 60px">
-          
-          <p><?php the_field('biography'); ?></p>  
-         
+        <div class="large-8 columns" style="padding: 60px">
+          <p><?php the_field('biography'); ?></p>
         </div>
-        
-          
-        
+        <div class="large-4 columns text-center " style="background: url('<?php the_field('headshot'); ?>');height: 300px;background-size:cover"></div>
+
       </div>
-      <?php endwhile; // end of the loop. ?>
+      <?php endwhile; ?>
     </div>
   </div>
 
