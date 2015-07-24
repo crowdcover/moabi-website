d6c71e218933055c4030a36dd4283bf3eaf31fc3
Stadl0r <erik@flintgame.com>
2015-07-24 10:37:04 -0400

    Reports page fix

--- single-reports.php
+++ single-reports.php
@@ -11,10 +11,6 @@
     <h1><?php the_title(); ?></h1>
     <p class="headline"></p>
   </div>
-
-</div>
-<div class="content" style="background: url('<?php the_field('project_bg'); ?>');background-size:cover;">
-
   <div class="row" style="background: #fff; padding: 60px">
     <div class="large-12 columns">
       <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
