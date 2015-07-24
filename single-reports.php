052c7787caafbf3940a06e45d55fe79f7a101f78
Stadl0r <erik@flintgame.com>
2015-07-24 10:46:42 -0400

    Report page fix

--- single-reports.php
+++ single-reports.php
@@ -10,7 +10,7 @@
   <div class="row text-center wp2">
     <h1><?php the_title(); ?></h1>
   </div>
-  <div class="row" style="background: #fff; padding: 60px">
+  <div class="row">
     <div class="large-12 columns">
       <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
