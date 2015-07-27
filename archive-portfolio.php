f9293536994d1f162a76db1d8c7ac304d0ea8b8f
Stadl0r <erik@flintgame.com>
2015-07-27 11:08:15 -0400

    projects archive page

--- archive-portfolio.php
+++ archive-portfolio.php
@@ -1,23 +1,34 @@
 <?php get_header(); ?>
 
+<div class="headerpg" ></div>
 
-<div class="container animated fadeIn delay-1s">
-		
-		<?php while ( have_posts() ) : the_post(); ?>
-			<a href="<?php echo get_permalink(); ?>">
-			<div class="company" style="background-image: url('<?php the_field('company_logo'); ?>')">
-				  <?php if( get_field('year_acquired') ): ?>
+</div>
+<div class="content" id="intro">
+  <div class="row text-center wp2">
+    <h1>Projects</h1>
+    <p class="headline">Moabi has worked on a diverse range of projects with both local and international partners. Below are short overviews of our most recent projects. If you’d like to work with us on your project, <a href="mailto:info@crowdcover.org">get in touch!</a> </p>
+  </div>
 
-                        <div class="ac">ACQUIRED</div>
+</div>
+<div class="work">
 
+<?php
+if ( get_query_var('paged') ) $paged = get_query_var('paged');
+if ( get_query_var('page') ) $paged = get_query_var('page');
 
+$query = new WP_Query( array( 'post_type' => 'portfolio', 'paged' => $paged ) );
 
-                    <?php endif; ?>
-
-			</div>
-			</a>
-   		<?php endwhile; // end of the loop. ?>
-        
+if ( $query->have_posts() ) : ?>
+  <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+    <figure class="large-4 columns" style="background: url('<?php the_field('project_logo'); ?>');background-size: cover;background-position:center;height:450px">
+      <a href="<?php the_permalink() ?>"><figcaption>
+      <h3>'<?php the_field('project_excerpt'); ?></h3>
+      </figcaption></a>
+      <?php the_content(); ?>
+    </figure>
+  <?php endwhile; wp_reset_postdata(); ?>
+<?php else : ?>
+<?php endif; ?>
 
 </div>
 
