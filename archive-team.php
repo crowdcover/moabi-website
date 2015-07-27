6d38288a68d8a3c232ded4094f5f57439a4bdc88
Stadl0r <erik@flintgame.com>
2015-07-27 14:15:40 -0400

    sub category for team

--- archive-team.php
+++ archive-team.php
@@ -9,8 +9,29 @@
     <p class="headline">We are a team of cartographers, coders, forest monitors, GIS experts, gender specialists and econometric modelers. Our team spans three continents and works to build simple yet robust technology, train local organizations,<br> and expand our partnerships.   </p>
   </div>
   <div class="row">
+  	<h2 class="text-center">Washington DC team</h2>
     <div class="large-12 columns">
      <?php query_posts('post_type=team&category_name=washington-dc'); while ( have_posts() ) : the_post(); ?>
+	      <div class="divider"></div>
+		  <div class="row grey collapse">
+		    <div class="large-6 columns" style="padding: 60px">
+		      <h2><?php the_title(); ?></h2>
+		      <p><span><?php the_field('company_role'); ?></span></p>
+		      <p><?php the_field('bio_excerpt'); ?></p>
+		      <p><a href="<?php echo get_permalink(); ?>">Read More</a></p>
+
+		    </div>
+		    <div class="large-6 columns text-center " style="background: url('<?php the_field('headshot'); ?>');height: 400px;background-size:cover">
+
+		    </div>
+		  </div>
+   		<?php endwhile; // end of the loop. ?>
+    </div>
+  </div>
+  <div class="row">
+  	<h2 class="text-center">DRC team</h2>
+    <div class="large-12 columns">
+     <?php query_posts('post_type=team&category_name=drc-team'); while ( have_posts() ) : the_post(); ?>
 
 
 	      <div class="divider"></div>
