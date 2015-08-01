ab67202b2a1d466b5fd96e145111debae08a47cd
cyrus4104 <cyrus.sinai@gmail.com>
2015-07-31 23:19:42 -0700

    Update archive-reports.php

--- archive-reports.php
+++ archive-reports.php
@@ -37,7 +37,7 @@
 
 <!-- REMOVE THIS SECTION BELOW, JUST PLACEHOLDER CONTENT -->
 
-	<figure class="large-6 columns featr" style="background: url('http://178.62.69.5/wp-content/uploads/2015/06/palm2-1024x768.jpg');background-size: cover; background-position: bottom;">
+	<figure class="large-6 columns featr" style="background: url('http://moabi.org/wp-content/uploads/2015/08/palm_suitability_w_legend.jpg');background-size: cover; background-position: bottom;">
       <a href="http://rdc.moabi.org/palm-oil-boom/en/#5/-2.877/22.830&layers=moabi_forest_cover" target="_blank"><figcaption>
         <h3>Palm Oil Boom?</h3>
         <p class="headline">IIASA scientists map where palm oil could expand in the Democratic Republic of Congo to become a major palm oil producer</p>
